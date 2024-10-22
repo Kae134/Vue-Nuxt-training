48a8c6c68c2e7bfe8289df97b6368e216136a570
Louis <louisdondey@gmail.com>
2024-10-22 11:19:31 +0200

    ajout des 1eres lignes de code

---
+++ Entrainement/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtLoadingIndicator />
+    <NuxtRouteAnnouncer />
+    <Header/>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+const config = useAppConfig();
+useSeoMeta({
+  title: config.title
+})
+
+</script>
---
+++ Entrainement/pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>BLOG</h1>
+    <article v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>{{  post.body.slice(0,1000) }}</p>
+        <p>
+            <NuxtLink :to="`/blog/${post.id}`">Lire plus</NuxtLink>
+        </p>
+    </article>
+</template>
+
+<script setup>
+const { data: posts } = await useFetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+
+</script>
